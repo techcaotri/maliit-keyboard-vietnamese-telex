402cd1bc0402a9d3b162a8919861ec23cd8acf23
Tri Pham <techcaotri@gmail.com>
2024-10-03 00:29:55 +0700

    Disable build test

--- build.sh
+++ build.sh
@@ -1,7 +1,7 @@
 #!/bin/env bash
 
 SCRIPT_DIR=$(cd -- "$(dirname -- "${BASH_SOURCE[0]}")" &>/dev/null && pwd)
-export CMAKE_PREFIX_PATH=/home/tripham/Qt/qt6-7-3_install
+export CMAKE_PREFIX_PATH=~/Qt/qt6-7-3_install
 export MaliitPlugins_DIR=$SCRIPT_DIR/../framework/framework_install/lib/cmake/MaliitPlugins/
 echo $MaliitPlugins_DIR
 
@@ -10,6 +10,6 @@ if [ ! -d build ]; then
 fi
 
 cd build
-script -qec "cmake -DCMAKE_EXPORT_COMPILE_COMMANDS=ON -DCMAKE_INSTALL_PREFIX=$(pwd)/../keyboard_install/ .. 2>&1" /dev/null | tee cmake_output.txt
+script -qec "cmake -Denable-tests=OFF -DCMAKE_EXPORT_COMPILE_COMMANDS=ON -DCMAKE_INSTALL_PREFIX=$(pwd)/../keyboard_install/ .. 2>&1" /dev/null | tee cmake_output.txt
 script -qec "cmake --build . --config Debug --verbose 2>&1" /dev/null | tee build_output.txt
 script -qec "cmake --install . --prefix $(pwd)/../keyboard_install --verbose 2>&1" /dev/null | tee install_output.txt
