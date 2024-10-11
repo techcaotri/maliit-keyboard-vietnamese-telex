cb8c5f720dc927aac4da2fc3b8ac14369104e7e2
Tri Pham <techcaotri@gmail.com>
2024-10-11 10:26:42 +0700

    Improve ngram database for Vietnamese IME

--- add_languages.sh
+++ add_languages.sh
@@ -1,4 +1,4 @@
 #!/bin/env bash
 
-echo "Adding the following languages to the keyboard; en, vi, ko, zh-hant, zh-hans, emoji"
+echo "Adding the following languages to the keyboard; en, vi, ko, zh-hant, zh-hans"
 gsettings set org.maliit.keyboard.maliit enabled-languages "['en', 'vi', 'ko', 'zh-hant', 'zh-hans']"
