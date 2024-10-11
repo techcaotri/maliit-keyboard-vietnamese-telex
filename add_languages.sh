ae30ddfb01e1a58f2dd0511c1e4f812e68e1249f
Tri Pham <techcaotri@gmail.com>
2024-10-11 09:54:17 +0700

    Add 'add_languages.sh' script to support the following Asian languages: 'ko', 'vi', 'zh-hant', 'zh-hans'

---
+++ add_languages.sh
@@ -0,0 +1,4 @@
+#!/bin/env bash
+
+echo "Adding the following languages to the keyboard; en, vi, ko, zh-hant, zh-hans, emoji"
+gsettings set org.maliit.keyboard.maliit enabled-languages "['en', 'vi', 'ko', 'zh-hant', 'zh-hans']"
